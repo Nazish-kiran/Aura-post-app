6e11ebc4c62a83c4c6cbc46a71ccef97f70e7033
Nazish-Kiran <nazishkirancosmos123@gmail.com>
2024-11-22 02:12:04 +0500

    made home.js

--- public/app.js
+++ public/app.js
@@ -18,104 +18,90 @@ const signEmail = document.querySelector(".email2");
 const signPassword = document.querySelector(".pass2");
 const confirmPassword = document.querySelector(".pass3");
 
-if (window.location.href == "https://aura-posting-web.web.app/") {
-  const changedUrl = () => {
-    window.location.replace("https://aura-posting-web.web.app/home.html");
-    console.log("done");
-  };
-  const createUser = (e) => {
-    let email = signEmail.value;
-    let password = signPassword.value;
-    let confPassword = confirmPassword.value;
-    if (password == confPassword) {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          console.log(userCredential);
-          const user = userCredential.user;
-          console.log(user);
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log(errorMessage);
-        });
-    } else {
-      alert("passwords dont match");
-    }
-  };
-  const signInUser = (e) => {
-    let email = useremail.value;
-    let password = userpassword.value;
-
-    signInWithEmailAndPassword(auth, email, password)
+const changedUrl = () => {
+  window.location.replace("https://aura-posting-web.web.app/home.html");
+  console.log("done");
+};
+const createUser = (e) => {
+  let email = signEmail.value;
+  let password = signPassword.value;
+  let confPassword = confirmPassword.value;
+  if (password == confPassword) {
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
+        console.log(userCredential);
         const user = userCredential.user;
         console.log(user);
-
-        console.log("exist");
-        localStorage.setItem(
-          "user",
-          JSON.stringify({ email: user.email, id: user.uid })
-        );
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorMessage);
       });
-    setTimeout(changedUrl, 2000);
-  };
-  const googleUser = (e) => {
-    console.log("Google Sign-In Button Clicked");
-    e.preventDefault();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const user = result.user;
-        console.log(user);
-        localStorage.setItem(
-          "user",
-          JSON.stringify({
-            email: user.email,
-            id: user.uid,
-            name: user.displayName || "Anonymous",
-            picture: user.photoURL,
-          })
-        );
-        console.log(user);
+  } else {
+    alert("passwords dont match");
+  }
+};
+const signInUser = (e) => {
+  let email = useremail.value;
+  let password = userpassword.value;
 
-        setTimeout(changedUrl, 2000);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
-      });
-  };
-  submittBtn.addEventListener("click", signInUser);
-  signBtn.addEventListener("click", createUser);
-  googleBtn.addEventListener("click", googleUser);
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+
+      console.log("exist");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ email: user.email, id: user.uid })
+      );
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  setTimeout(changedUrl, 2000);
+};
+const googleUser = (e) => {
+  console.log("Google Sign-In Button Clicked");
+  e.preventDefault();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(user);
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          email: user.email,
+          id: user.uid,
+          name: user.displayName,
+          picture: user.photoURL,
+        })
+      );
+      console.log(user);
+
       setTimeout(changedUrl, 2000);
-      // document.write(user);
-      // console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+};
+submittBtn.addEventListener("click", signInUser);
+signBtn.addEventListener("click", createUser);
+googleBtn.addEventListener("click", googleUser);
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    setTimeout(changedUrl, 2000);
+    const uid = user.uid;
+  } else {   
+  }
+});
 
-      const uid = user.uid;
-      // ...
-    } else {
-      // User is signed out
-      // ...
-    }
-  });
-} else if (window.location.href == "https://aura-posting-web.web.app/home.html") {
-  var profilePicture = document.querySelector(".profile-pict");
-  const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user.name, user.picture);
-  var myProfilePicture = user.picture;
-  console.log(profilePicture);
-
-  profilePicture.src = myProfilePicture;
-}
+export { auth, onAuthStateChanged };
---
+++ public/home.js
@@ -0,0 +1,14 @@
+import { auth, onAuthStateChanged } from "app.js";
+
+onAuthStateChanged( (user)=> {
+    console.log(user);
+    
+  if (user) {
+    const profilePicture = document.querySelector(".profile-pict");
+    const displayName = document.querySelector(".dp-name-p");
+    displayName.innerHTML = user.displayName || "Anonymous"; 
+    profilePicture.src = user.photoURL || "default-pic.jpg";
+  } else {   
+    window.location.href = "login.html";
+  }
+});
