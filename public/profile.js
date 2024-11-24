197f4b938a950dade379703162021bc37c3bb8f6
Nazish-Kiran <nazishkirancosmos123@gmail.com>
2024-11-25 01:36:22 +0500

    added signout firebase

--- public/profile.js
+++ public/profile.js
@@ -1,9 +1,19 @@
+import { auth, signOut, onAuthStateChanged } from "./firebase.js";
+
 const logOutBtn = document.querySelector(".logOut-btn");
 
 const logOut= ()=>{
-  window.location.replace("https://aura-posting-web.web.app/index.html");
-  window.location.replace("index.html");
-  localStorage.clear()
+localStorage.clear()
+signOut(auth)
+.then(() => {
+    console.log("User logged out successfully");
+    // Redirect to the main page or login page
+    window.location.replace("index.html");
+    // window.location.href = "https://aura-posting-web.web.app";
+    })
+    .catch((error) => {
+      console.error("Error during logout:", error.message);
+    });
 }
 
 logOutBtn.addEventListener("click",logOut)
